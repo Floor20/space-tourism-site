58fc818c34149852b13696cb5c5e19a78479eb72
Floor20 <austino2112@gmail.com>
2023-08-31 15:32:09 -0600

    Completed destination page

---
+++ src/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const navToggleBtn = document.querySelector(".mobile-nav-toggle");
+const nav = document.getElementById("primary-navigation");
+
+console.log(navToggleBtn);
+
+//EVENT LISTENER
+navToggleBtn.addEventListener("click", (e) => {
+  const visibility = nav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggleBtn.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggleBtn.setAttribute("aria-expanded", false);
+  }
+
+  console.log(navToggleBtn.getAttribute("aria-expanded"));
+});
