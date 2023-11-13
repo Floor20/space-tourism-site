abb0a3494145629e291c566730f20ac4033c18cd
Floor20 <austino2112@gmail.com>
2023-11-12 20:00:33 -0700

    Destination page w/tab functionality

--- src/tabs.js
+++ src/tabs.js
@@ -8,42 +8,56 @@ let tabFocus = 0;
 // navigates focus with keyboard
 tabList.addEventListener("keydown", changeTabFocus);
 
+// activates focused tab on click
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
+
 function changeTabFocus(e) {
   const keydownLeft = 37;
   const keydownRight = 39;
 
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) tabFocus = 0;
-  }
-
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) tabFocus = tabs.length - 1;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) tabFocus = 0;
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) tabFocus = tabs.length - 1;
+    }
   }
 
   tabs[tabFocus].setAttribute("tabindex", 0);
   tabs[tabFocus].focus();
 }
 
-// activates focused tab on "enter"
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const targetTab = e.target;
-    const targetPanel = targetTab.getAttribute("aria-controls");
-    const tabContainer = targetTab.parentNode;
-    const mainContainer = tabContainer.parentNode;
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+  const targetImage = targetTab.getAttribute("data-image");
 
-    mainContainer
-      .querySelectorAll('[role="tabpanel"]')
-      .forEach((panel) => panel.setAttribute("hidden", true));
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+  targetTab.setAttribute("aria-selected", true);
 
-    console.log(tabContainer);
-  });
-});
+  hideContent(mainContainer, '[role="tabpanel"]');
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, targetPanel);
+  showContent(mainContainer, targetImage);
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector([`#${content}`]).removeAttribute("hidden");
+}
